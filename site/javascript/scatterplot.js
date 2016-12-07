cccc88a64935c8189fa8089fb7c82d0c40397198
Sharif Jacobino <sharif_jacobino@hotmail.com>
2016-12-07 08:18:47 +0100

    Nice scatter plot

--- site/javascript/scatterplot.js
+++ site/javascript/scatterplot.js
@@ -1,30 +1,100 @@
-var viewWidth = window.innerWidth;
-var viewHeight = window.innerHeight;
+var viewWidth = 500;
+var viewHeight = 450;
 d3.select(window).on("resize", resize);
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40};
 var width = viewWidth - margin.left - margin.right;
 var height = viewHeight - margin.top - margin.bottom;
 
-var svg = d3.select("svg")
+var svg = d3.select("#d3_svg")
     .attr("width", viewWidth)
     .attr("height", viewHeight)
+    .attr("margin", "auto")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-function drawScatterplot() {
-  //You can implement your scatterplot here
+var drawn = false;
 
+function drawScatterplot(year) {
+  var year = (year - 2005) * 5;
+	var boats = boat_data.boats; 
+  //You can implement your scatterplot here
 
   //The svg is already defined, you can just focus on the creation of the scatterplot
   //you should at least draw the data points and the axes.
-  console.log("Draw Scatterplot");
+  console.log("Draw Scatterplot " + year);
 
   //The data can be found in the boat_data.boats variable
-  console.log(boat_data.boats); //IMPORTANT - Remove this, it is here just to show you how to access the data
+  //console.log(boat_data.boats); //IMPORTANT - Remove this, it is here just to show you how to access the data
 
   //You can start with a simple scatterplot that shows the x and y attributes in boat_data.boats
+  
+  // used http://bl.ocks.org/weiglemc/6185069
+  // used http://bl.ocks.org/mbostock/3887118
 
+  //var circles = svg.selectAll(".dot").data([]); //.selectAll('circles');
+    
+  //circles.exit().remove();
+	   
+	var x = d3.scale.linear()
+		.range([0, width]);
+	var y = d3.scale.linear()
+		.range([height, 0]);
+	var color = d3.scale.category10();
+	
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left");
+		
+	x.domain(d3.extent(boats, function(d) { return d.x; })).nice();
+	y.domain(d3.extent(boats, function(d) { return d.y; })).nice();
+	
+	svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("x values");
+
+	svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("y values")
+	
+	dots = svg.selectAll(".dot")
+		.data(boats);
+
+  if (drawn) {
+    dots.transition()
+    .duration(1000)
+    .attr("class", "dot")
+    .attr("r", 3.5)
+    .attr("cx", function(d) {return x(d.x - year);})
+    .attr("cy", function(d) {return y(d.y - year * 2);})
+    .style("fill", color(1.0));
+  } else {
+		dots.enter().append("circle")
+		.attr("class", "dot")
+		.attr("r", 3.5)
+		.attr("cx", function(d) {return x(d.x - year);})
+		.attr("cy", function(d) {return y(d.y - year * 2);})
+		.style("fill", color(1.0));
+    drawn = true;
+  }
+		
   //Additional tasks are given at the end of this file
 }
 
@@ -36,7 +106,7 @@ function resize() {
 }
 
 
-drawScatterplot();
+drawScatterplot(2006);
 
 
 //////////////////////////////////////////////////////////////////////////////////
--- site/javascript/slider.js
+++ site/javascript/slider.js
@@ -2,3 +2,8 @@ function showValue(newValue)
 {
     document.getElementById("range").innerHTML=newValue;
 }
+
+function updateVisualizations(newValue)
+{
+    drawScatterplot(newValue);
+}
