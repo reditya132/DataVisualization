3421c93c000cde7ed9b71b8bd2e1095829c623a2
Ruben Vroegindeweij <ruben_vroegindeweij@hotmail.com>
2016-12-22 15:05:30 +0100

    Replaces code and adds comments

--- InfoVis/site/javascript/select.js
+++ InfoVis/site/javascript/select.js
@@ -1,20 +1,24 @@
+// Loads the data and meta data of the Amsterdam dataset.
 d3.queue()
-	.defer(d3.tsv, "../datasets/data_tab_delimited.txt")
 	.defer(d3.tsv, "../datasets/metadata.txt")
 	.await(ready);
 
-function ready(error, data, metadata) {
-	addSelect(data, metadata, "selectVariableA", "variableA");
-	addSelect(data, metadata, "selectVariableB", "variableB")
+// When the data has been loaded selections will be showed for the two variables.
+function ready(error, metadata) {
+	addSelect(metadata, "selectVariableA", "variableA");
+	addSelect(metadata, "selectVariableB", "variableB");
 }
 
-function addSelect(data, metadata, divID, selectID){
+// Append a variable selection to an element.
+function addSelect(metadata, divID, selectID){
 	// Shows the metadata in a select element.
 	var thema;
 	var selectHtml = "";
 	selectHtml += "<select id = " + selectID +  " size='20'>" ;
 	metadata.forEach(function(d){
+		// Variables that are not measured once a year will not be option in the selec element.
 		if(d.Verschijningsfrequentie == "1 keer per jaar"){
+			// If this variable has another thema a new group will be created.
 			if(d.THEMA !== thema){
 				if(thema !== undefined){
 					selectHtml += "</optgroup>";
@@ -22,10 +26,11 @@ function addSelect(data, metadata, divID, selectID){
 				thema = d.THEMA;
 				selectHtml += "<optgroup label='" + thema + "'>";
 			}
+			// A new select option gets created per variable with value, title and label from metadata..
 			selectHtml += "<option value='" +d.Variabele.replace(" ","") + "'" + "title='" + d.Definitie + "'" + ">" + d.Label + "</option>";
 		}
 	});
-	selectHtml +=  "</select>" ;
+	selectHtml +=  "</optgroup></select>" ;
 	$( "#" + divID ).append( selectHtml );
 	$("#" + selectID).chosen();
 }
